eef5777ac9056b14b8b0bcda0250b44589ef0bf4
Raghu-M <442raghu@gmail.com>
2017-05-06 21:33:09 +0530

    added web client
    added web client

---
+++ journal-web/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { RouterModule }   from '@angular/router';
+import { HttpModule }    from '@angular/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroService } from './hero.service';
+import { HeroSearchComponent } from './hero-search.component';
+
+import { AppRoutingModule }     from './app-routing.module';
+
+
+@NgModule({
+  imports:[ 
+  	BrowserModule, 
+  	FormsModule,
+  	AppRoutingModule,
+  	HttpModule,
+  	InMemoryWebApiModule.forRoot(InMemoryDataService),
+  ],
+  declarations: [ 
+  	AppComponent, 
+  	HeroDetailComponent, 
+  	HeroesComponent,
+  	DashboardComponent,
+  	HeroSearchComponent
+  ],
+  bootstrap:    [ AppComponent ],
+  providers: [ HeroService ]
+})
+export class AppModule { }

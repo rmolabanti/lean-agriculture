a56fb9429d8b8d424323490033f86d187a940c2e
rmolabanti <rmolabanti@reputation.com>
2018-09-08 11:16:09 +0530

    update material ui

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CustomizedTable from './Table';
+import Grid from '@material-ui/core/Grid';
 
 class App extends Component {
   render() {
@@ -9,12 +10,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <CustomizedTable/>
+          <h1 className="App-title">Welcome to Lean Agriculture</h1>
+        </header>        
+        <Grid container spacing={16} justify="center" style={{padding: 24}}>
+        <Grid item  spacing={16}>
+          <CustomizedTable/>
+        </Grid>
+        </Grid>
       </div>
     );
   }
